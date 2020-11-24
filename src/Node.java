1e0fc2cea9a4e74044d814d8f373241b726611fd
Nilstrieb <nils.heydecker@gmx.ch>
2020-11-24 21:14:43 +0100

    created empty classes

---
+++ src/Node.java
@@ -0,0 +1,15 @@
+public abstract class Node {
+
+    private int id;
+    private Node parent;
+
+    public int getId() {
+        return 0;
+    }
+
+    public Node(int id) {
+
+    }
+
+    public abstract double getValue();
+}
