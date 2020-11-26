9955cb92f6bd5e0bece56ed72a8f01a6e59335b6
Nilstrieb <nils.heydecker@gmx.ch>
2020-11-26 07:58:13 +0100

    fixed bugs

--- src/nodes/BracketNode.java
+++ src/nodes/BracketNode.java
@@ -11,75 +11,79 @@ public class BracketNode extends ValueNode {
     /**
      * All operators in the expression
      */
-    private final ArrayList<OperatorNode> operators;
+    private ArrayList<OperatorNode> operators;
 
     /**
      * Create a new BracketNode from the content of the brackets (without the brackets)
      * @param line The content
      * @throws NumberFormatException If there is something wrong
      */
-    public BracketNode(String line) throws NumberFormatException{
+    public BracketNode(String line) throws NumberFormatException {
+
         operators = new ArrayList<>();
 
-        ValueNode leftNumber = new ValueNode();
-        ValueNode rightNumber = new ValueNode();
-        char currentOperator = '0';
-        int bracketLevel = 0;
-        String bracketContent = "";
+        if (!line.matches("[\\d\\w]*")) {
 
-        for (int i = 0; i < line.length(); i++) {
+            ValueNode leftNumber = new ValueNode();
+            ValueNode rightNumber = new ValueNode();
+            char currentOperator = '0';
+            int bracketLevel = 0;
+            String bracketContent = "";
 
-            char c = line.charAt(i);
+            for (int i = 0; i < line.length(); i++) {
 
-            if (bracketLevel == 0 || c == ')' || c == '(') {
-                switch (c) {
-                    case '+':
-                    case '-':
-                    case '*':
-                    case '/':
-                        if (currentOperator != '0') {
-                            OperatorNode newOperator = new OperatorNode(leftNumber, rightNumber, currentOperator);
-                            operators.add(newOperator);
-                            leftNumber.addParent(newOperator);
-                            rightNumber.addParent(newOperator);
-                            leftNumber = rightNumber;
-                            rightNumber = new ValueNode();
-                        }
-                        currentOperator = c;
-                        break;
-                    case ' ':
-                        break;
-                    case '(':
-                        bracketLevel++;
-                        break;
-                    case ')':
-                        bracketLevel--;
-                        if (bracketLevel == 0) {
-                            if (currentOperator == '0') {
-                                leftNumber = new BracketNode(bracketContent);
-                            } else {
-                                rightNumber = new BracketNode(bracketContent);
+                char c = line.charAt(i);
+
+                if (bracketLevel == 0 || c == ')' || c == '(') {
+                    switch (c) {
+                        case '+':
+                        case '-':
+                        case '*':
+                        case '/':
+                            if (currentOperator != '0') {
+                                OperatorNode newOperator = new OperatorNode(leftNumber, rightNumber, currentOperator);
+                                operators.add(newOperator);
+                                leftNumber.addParent(newOperator);
+                                rightNumber.addParent(newOperator);
+                                leftNumber = rightNumber;
+                                rightNumber = new ValueNode();
                             }
-                            bracketContent = "";
-                        }
-                        break;
+                            currentOperator = c;
+                            break;
+                        case ' ':
+                            break;
+                        case '(':
+                            bracketLevel++;
+                            break;
+                        case ')':
+                            bracketLevel--;
+                            if (bracketLevel == 0) {
+                                if (currentOperator == '0') {
+                                    leftNumber = new BracketNode(bracketContent);
+                                } else {
+                                    rightNumber = new BracketNode(bracketContent);
+                                }
+                                bracketContent = "";
+                            }
+                            break;
 
-                    default:
-                        if (currentOperator == '0') {
-                            leftNumber.append(c);
-                        } else {
-                            rightNumber.append(c);
-                        }
+                        default:
+                            if (currentOperator == '0') {
+                                leftNumber.append(c);
+                            } else {
+                                rightNumber.append(c);
+                            }
+                    }
+                } else {
+                    bracketContent += c;
                 }
-            } else {
-                bracketContent += c;
             }
-        }
 
-        OperatorNode newOperator = new OperatorNode(leftNumber, rightNumber, currentOperator);
-        operators.add(newOperator);
-        leftNumber.addParent(newOperator);
-        rightNumber.addParent(newOperator);
+            OperatorNode newOperator = new OperatorNode(leftNumber, rightNumber, currentOperator);
+            operators.add(newOperator);
+            leftNumber.addParent(newOperator);
+            rightNumber.addParent(newOperator);
+        }
     }
 
 
@@ -99,7 +103,7 @@ public class BracketNode extends ValueNode {
             boolean hasPriorityNodes = true;
             while (hasPriorityNodes && operatorsCopy.size() > 1) {
                 hasPriorityNodes = false;
-                for (int i = 0; i < operatorsCopy.size() - 1; i++) {
+                for (int i = 0; i < operatorsCopy.size() && operatorsCopy.size() > 1; i++) {
                     OperatorNode op = operatorsCopy.get(i);
                     if (op.hasPriority()) {
                         op.calculate();
